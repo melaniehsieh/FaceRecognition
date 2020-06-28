a195ab1f533891f0c2b962916a3949a775eef880
melanie hsieh <mpwh2004@gmail.com>
2020-06-28 19:35:46 +0800

    v1 launch

--- js/main.js
+++ js/main.js
@@ -6,50 +6,52 @@ Promise.all([
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
   faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-  faceapi.nets.ageGenderNet.loadFromUri("/models")
+  faceapi.nets.ageGenderNet.loadFromUri("/models"),
 ]).then(startVideo);
 
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
-    stream => (video.srcObject = stream),
-    err => console.error(err)
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
   );
 }
 
 video.addEventListener("playing", () => {
   const canvas = faceapi.createCanvasFromMedia(video);
-  document.body.append(canvas);
+  let container = document.querySelector(".container");
+  container.append(canvas);
 
   const displaySize = { width: video.width, height: video.height };
   faceapi.matchDimensions(canvas, displaySize);
 
   setInterval(async () => {
     const detections = await faceapi
-      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
       .withFaceExpressions()
       .withAgeAndGender();
+
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
 
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
 
     faceapi.draw.drawDetections(canvas, resizedDetections);
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-
-    const age = resizedDetections[0].age;
-    const interpolatedAge = interpolateAgePredictions(age);
-    const bottomRight = {
-      x: resizedDetections[0].detection.box.bottomRight.x - 50,
-      y: resizedDetections[0].detection.box.bottomRight.y
-    };
-
-    new faceapi.draw.DrawTextField(
-      [`${faceapi.utils.round(interpolatedAge, 0)} years`],
-      bottomRight
-    ).draw(canvas);
-  }, 100);
+    if (resizedDetections && Object.keys(resizedDetections).length > 0) {
+      const age = resizedDetections.age;
+      const interpolatedAge = interpolateAgePredictions(age);
+      const gender = resizedDetections.gender;
+      const expressions = resizedDetections.expressions;
+      const maxValue = Math.max(...Object.values(expressions));
+      const emotion = Object.keys(expressions).filter(
+        (item) => expressions[item] === maxValue
+      );
+      document.getElementById("age").innerText = `Age - ${interpolatedAge}`;
+      document.getElementById("gender").innerText = `Gender - ${gender}`;
+      document.getElementById("emotion").innerText = `Emotion - ${emotion}`;
+    }
+  }, 10);
 });
 
 function interpolateAgePredictions(age) {
